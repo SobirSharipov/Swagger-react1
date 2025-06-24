a5c5315f2064a612bb5b6ecbb73a82b03e2ba0ff
Пользователь <sobirjonsharipov590@gmail.com>
2025-06-24 13:03:16 +0500

    Swager - react 1

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -88,7 +88,7 @@ const Card = () => {
         setidx(el.id)
     }
 
-    let [Search,setSearch]=useState("")
+    let [Search, setSearch] = useState("")
 
     useEffect(() => {
         get()
@@ -98,7 +98,7 @@ const Card = () => {
 
     return (
         <div className='text-center my-[50px]'>
-          
+
             <Modal
                 title="Basic Modal"
                 closable={{ 'aria-label': 'Custom Close Button' }}
@@ -119,20 +119,30 @@ const Card = () => {
             </Modal>
             <h1 className='font-black text-3xl my-[60px]'>Новые поступления обуви</h1>
             <div className='flex justify-around items-center'>
-            <input className='lg:w-[30%] w-[50%] border p-[10px] rounded-[5px]' type="text" placeholder='Search...' value={Search} onChange={(e)=>setSearch(e.target.value)} />
-              <Button type="primary" onClick={showModal}>
-                Add New Producs
-            </Button>
+                <input className='lg:w-[30%] w-[50%] border p-[10px] rounded-[5px]' type="text" placeholder='Search...' value={Search} onChange={(e) => setSearch(e.target.value)} />
+                <Button type="primary" onClick={showModal}>
+                    Add New Producs
+                </Button>
             </div>
             <div className='grid lg:grid-cols-4 grid-cols-2 my-[30px] w-[95%] mx-auto gap-[20px] mb-[50px]'>
-                {User.filter(el=>el.name.toLowerCase().includes(Search.toLowerCase())).map(el => {
+                {User.filter(el => el.name.toLowerCase().includes(Search.toLowerCase())).map(el => {
                     return (
                         <div key={el.id} className='border  p-[20px]'>
                             <img src={img6} alt="" />
                             <h1>{el.name}</h1>
-                            <div className='flex justify-around my-[10px]'>
-                                <Button type="primary" className='border px-[10px]' onClick={() => DeletUser(el.id)}>Delete</Button>
-                                <Button type="primary" className='border px-[10px]' onClick={(e) => Edituser(el)} >Edit</Button>
+                            <div className='flex justify-around gap-[20px] my-[10px]'>
+                                <button type="primary" className='border px-[10px] text-red-600' onClick={() => DeletUser(el.id)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
+
+                                </button>
+                                <button type="primary" className='border px-[10px] text-blue-600' onClick={(e) => Edituser(el)} >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                    </svg>
+
+                                </button>
                             </div>
                         </div>
                     )
